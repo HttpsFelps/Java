d57ee40d02de0fd0e76ab7711e7870d638bf46a4
Felps <luis.foresto@fatec.sp.gov.br>
2024-10-28 18:55:41 -0300

    correção de estrutura

--- tipos-variaveis/src/Metodos.java
+++
@@ -1,6 +0,0 @@
-public class Metodos {
-    //deve ser nomeado com verbos, segir o padrao camel case
-    //acharPeloId(int Id){   }
-    //caso nao retorne nada sera do tipo void
-    //podemos utilizar parametros
-}
