6edecb9b268aebb5edf82a72c952fc32c21cb4c2
HttpsFelps <luisf.foresto@gmail.com>
2025-06-19 03:31:19 -0300

    Entendendo conceitos basicos de Atribuição e referência

---
+++ estrutura-de-dados/src/main/java/com/exemplo/Obj.java
@@ -0,0 +1,24 @@
+package com.exemplo;
+
+public class Obj {
+    private Integer num;
+
+    public Obj(Integer num) {
+        this.num = num;
+    }
+
+    public Obj(){super();}
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+
+    @Override
+    public String toString() {
+        return this.num.toString();
+    }
+}
